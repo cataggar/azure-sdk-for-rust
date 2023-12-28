16756bb3332b3deefc6a1c7e0a4e5e24c10be212
Cameron Taggart <cataggar@microsoft.com>
2023-12-28 12:02:10 -0600

    switch back 3 more

--- sdk/identity/src/token_credentials/mod.rs
+++ sdk/identity/src/token_credentials/mod.rs
@@ -8,30 +8,30 @@
 mod app_service_managed_identity_credential;
 mod azure_cli_credentials;
 #[cfg(feature = "azureauth-cli")]
-mod azureauth_cli_credential;
+mod azureauth_cli_credentials;
 mod cache;
 #[cfg(feature = "client_certificate")]
 mod client_certificate_credential;
-mod client_secret_credential;
+mod client_secret_credentials;
 mod default_credentials;
 mod environment_credentials;
 mod imds_managed_identity_credentials;
 mod options;
 mod specific_azure_credential;
 mod virtual_machine_managed_identity_credential;
-mod workload_identity_credential;
+mod workload_identity_credentials;
 
 pub use app_service_managed_identity_credential::*;
 pub use azure_cli_credentials::*;
 #[cfg(feature = "azureauth-cli")]
-pub use azureauth_cli_credential::*;
+pub use azureauth_cli_credentials::*;
 #[cfg(feature = "client_certificate")]
 pub use client_certificate_credential::*;
-pub use client_secret_credential::*;
+pub use client_secret_credentials::*;
 pub use default_credentials::*;
 pub use environment_credentials::*;
 pub use imds_managed_identity_credentials::*;
 pub use options::*;
 pub use specific_azure_credential::*;
 pub use virtual_machine_managed_identity_credential::*;
-pub use workload_identity_credential::*;
+pub use workload_identity_credentials::*;
