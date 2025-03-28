7a0a77cf719b963af4f8acb86ed2803c1d53596d
Cameron Taggart <cameron.tagg>
2025-03-28 12:53:30 +0100

    models check

--- services/autorust/codegen/src/codegen_operations/create_client_and_builder.rs
+++ services/autorust/codegen/src/codegen_operations/create_client_and_builder.rs
@@ -119,7 +119,7 @@ pub fn create_client(modules: &[String], endpoint: Option<&str>) -> Result<Token
                 self.scopes.iter().map(String::as_str).collect()
             }
             pub(crate) async fn send(&self, request: &mut azure_core::http::Request) -> azure_core::Result<azure_core::http::Response> {
-                let context = azure_core::Context::default();
+                let context = typespec_client_core::http::Context::default();
                 self.pipeline.send(&context, request).await
             }
 
